f4aa0b4d01708cc4dc093e3fe960b415ada164f0
tchiem0000 <33714268+tchiem0000@users.noreply.github.com>
2018-10-19 13:49:57 +0800

    Update app.js
    test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 var express     = require('express');
 var bodyParser  = require('body-parser');
 var errorhandler = require('errorhandler');
-var http        = require('http');
+var http        = require('http');  //web service
 var path        = require('path');
 var request     = require('request');
 var routes      = require('./routes');
@@ -41,3 +41,39 @@ app.post('/journeybuilder/execute/', activity.execute );
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+//test
+function getJSON(options, cb){	
+	http.requests(options, function(res){
+		var body = '';
+		
+		res.on('data', function(chunk){
+			bod += chunk;			
+		});
+		
+		res.on('end', function(){
+			var result = JSON.parse(body);
+			cb(null, result);				
+		});
+		
+		res.on('error', cb);
+	})
+	.on('error', cb)
+	.end();	
+}
+
+var options = {
+	host: 'api-sandbox.oanda.com',
+	port: 80,
+	path: '/v1/quote?instruments=USA_ZAR',
+	method: 'GET'	
+};
+
+getJSON(options, function(err, result){
+	if (err){
+		return console.log('Error while trying to get price: ', err);
+	}
+	
+	console.log(result);
+});
+
